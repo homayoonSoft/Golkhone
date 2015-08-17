1bc66c9a9b17cf27d57549fe98c785e9ff98fa04
homayoon <homayoon.ezabadi@gmail.com>
2015-08-17 16:45:54 -0400

    calss ymal reader

---
+++ connection.py
@@ -0,0 +1,5 @@
+import yaml_reader.py
+filepath = "config.yaml"
+yf = yaml_loader_dumper
+data=yf.yaml_loader(filepath)
+print (data)
--- yaml_reader.py
+++ yaml_reader.py
@@ -1,19 +1,23 @@
-import yaml
+class yld:
+        def __init__ (self):
+                data = {}
+                
+        def yaml_loader(self, filepath):
+                """Loads a yaml file"""
+                with open(filepath, "r") as file_descriptor:
+                        data = yaml.load(file_descriptor)
+                return data
 
-def yaml_loader(filepath):
-	"""Loads a yaml file"""
-	with open(filepath, "r") as file_descriptor:
-		data = yaml.load(file_descriptor)
-	return data
-
-def ymal_dump(filepath, data):
-	"""Dump data to a yaml file"""
-	with open(filepath, "w") as file_desxriptor:
-		yaml.dump(data, file_descriptor)
-
+        def ymal_dump(self, filepath, data):
+                """Dump data to a yaml file"""
+                with open(filepath, "w") as file_desxriptor:
+                        yaml.dump(data, file_descriptor)
+"""
 if __name__ == "__main__":
 	filepath = "config.yaml"
 	data = yaml_loader(filepath)
+	print (data)
 	dbInfo = data.get("dbInfo")
 	for item_name, item_value in dbInfo.items():
 		print (item_name, item_value)
+"""
